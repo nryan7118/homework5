7146ec900a99e3ec4554157e5e3eb4e5ed1cb3e1
nryan7118 <nickryan7118@gmail.com>
2024-04-30 13:54:23 -0400

    Initial commit

---
+++ TaskList/TaskList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskList
+//
+//  Created by Nick Ryan on 4/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskList/TaskList/TaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListApp.swift
+//  TaskList
+//
+//  Created by Nick Ryan on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
